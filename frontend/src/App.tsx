579e8d5d3bc80a2e06d115c8db183c83132330ed
JofreEscriba <jofre.escriba@mataro.epiaedu.cat>
2025-05-15 11:55:00 +0200

    Protecció de rutes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ import MyCards from './pages/MyCards';
 import Layout from './components/Layout';
 import Dashboard from "./pages/Dashboard";
 import PostCreatedPage from './pages/postCreatedPage';
+import ProtectedRoute from './components/ProtectedRoute'; // Importa el componente
 import './styles/global.css';
 
 const App: React.FC = () => {
@@ -17,15 +18,60 @@ const App: React.FC = () => {
     <Router basename="/Post-AR">
       <Routes>
         <Route element={<Layout />}>
+          {/* Rutas públicas */}
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/cardAdd" element={<CardAdd />} />
-          <Route path="/cardEdit/:id" element={<CardEdit />} />
-          <Route path="/MyCards" element={<MyCards />} />
-          <Route path="/post-created" element={<PostCreatedPage />} />
+
+          {/* Rutas protegidas */}
+          <Route 
+            path="/dashboard" 
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/profile" 
+            element={
+              <ProtectedRoute>
+                <ProfilePage />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/cardAdd" 
+            element={
+              <ProtectedRoute>
+                <CardAdd />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/cardEdit/:id" 
+            element={
+              <ProtectedRoute>
+                <CardEdit />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/MyCards" 
+            element={
+              <ProtectedRoute>
+                <MyCards />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/post-created" 
+            element={
+              <ProtectedRoute>
+                <PostCreatedPage />
+              </ProtectedRoute>
+            } 
+          />
         </Route>
       </Routes>
     </Router>
